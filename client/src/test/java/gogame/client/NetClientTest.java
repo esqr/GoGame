d0ce16343a590f1e423a092ef48cc0d8702dc552
esqr <tercjak@gmail.com>
2016-12-20 00:09:26 +0100

    update NetClientTest

--- client/src/test/java/gogame/client/NetClientTest.java
+++ client/src/test/java/gogame/client/NetClientTest.java
@@ -1,26 +1,43 @@
 package gogame.client;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.*;
+import java.net.ProtocolException;
 import java.net.Socket;
 
 import static org.junit.Assert.*;
-import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 public class NetClientTest {
-    @Test(timeout = 20000)
-    public void testClientWriting() throws Exception {
-        Socket socket = mock(Socket.class);
-        PipedOutputStream pos = new PipedOutputStream();
-        PipedInputStream is = new PipedInputStream(pos);
-        ByteArrayOutputStream os = new ByteArrayOutputStream();
+    private Socket socket = null;
+    private NetClient netClient = null;
+    private PipedOutputStream pos = null;
+    private PipedInputStream is = null;
+    private ByteArrayOutputStream os = null;
+
+    @Before
+    public void beforeEachTest() throws Exception {
+        socket = mock(Socket.class);
+        pos = new PipedOutputStream();
+        is = new PipedInputStream(pos);
+        os = new ByteArrayOutputStream();
         when(socket.getOutputStream()).thenReturn(os);
         when(socket.getInputStream()).thenReturn(is);
+    }
 
-        NetClient player = new NetClient(socket);
-        assertEquals("HELLO\nBYE\n", os.toString());
+    @Test(timeout = 1000)
+    public void testHello() throws Exception {
+        pos.write("HELLO\n".getBytes());
+        netClient = new NetClient(socket, null);
+        assertEquals("HELLO\n", os.toString());
+    }
+
+    @Test(timeout = 1000, expected = ProtocolException.class)
+    public void testBadHello() throws Exception {
+        pos.write("NOT A HELLO\n".getBytes());
+        netClient = new NetClient(socket, null);
     }
 }
