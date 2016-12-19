8eed0a4791fee5f83bd73480297627d3f60c1acc
esqr <tercjak@gmail.com>
2016-12-19 21:51:53 +0100

    add screen managing to MainWindowController

--- client/src/main/java/gogame/client/controllers/MainWindowController.java
+++ client/src/main/java/gogame/client/controllers/MainWindowController.java
@@ -1,10 +1,7 @@
 package gogame.client.controllers;
 
-import gogame.client.screenmanager.NoSuchScreenException;
-import gogame.client.screenmanager.ScreenManager;
-import gogame.client.screenmanager.Screens;
+import gogame.client.screenmanager.*;
 import javafx.fxml.FXML;
-
 import java.io.IOException;
 import java.net.URL;
 
@@ -12,8 +9,16 @@ public class MainWindowController {
     @FXML
     private ScreenManager screenManager;
 
-    public void setMainScreen(URL resource) throws IOException, NoSuchScreenException {
+    public void setMainScreen(URL resource) throws IOException, NoSuchScreenException, NoControllerException {
         screenManager.loadScreen(Screens.MAIN, resource);
         screenManager.setScreen(Screens.MAIN);
+    }
+
+    public void addScreen(String name, URL resource) throws IOException, NoControllerException {
+        screenManager.loadScreen(name, resource);
+    }
+
+    public ControlledScreen getScreen(String name) {
+        return screenManager.getScreen(name);
     }
 }
