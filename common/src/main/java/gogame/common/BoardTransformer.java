7201f639cdae8466c3a9098d8d1311e22af84dc4
Piotr Wójcik <chocimier@tlen.pl>
2016-12-22 23:50:49 +0100

    Allow reuse of getChain

--- common/src/main/java/gogame/common/BoardTransformer.java
+++ common/src/main/java/gogame/common/BoardTransformer.java
@@ -6,7 +6,6 @@ import java.util.List;
 import java.util.Queue;
 
 public class BoardTransformer {
-    private static final int[][] NEIGHBOURS = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
 
     public static Color[][] transform(int changedX, int changedY, Color[][] state) {
         final int[][] NEIGHBOURS_AND_SELF = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}, {0, 0}};
@@ -24,7 +23,7 @@ public class BoardTransformer {
                 int visitedY = changedY+adjacent[1];
 
                 if (!hasLiberty(visitedX, visitedY, resultState)) {
-                    List<int[]> chain = getChain(visitedX, visitedY, state);
+                    List<int[]> chain = Utils.getChain(visitedX, visitedY, state);
 
                     for (int[] stone: chain) {
                         resultState[stone[0]][stone[1]] = Color.NONE;
@@ -38,47 +37,12 @@ public class BoardTransformer {
         return resultState;
     }
 
-    private static List<int[]> getChain(int startX, int startY, Color[][] state) {
-        boolean[][] visited = new boolean[state.length][state[0].length];
-        List<int[]> sameChain = new ArrayList<int[]>();
-        Queue<int[]> queue = new LinkedList<int[]>();
-        int[] startPosition = {startX, startY};
-
-        visited[startX][startY] = true;
-        sameChain.add(startPosition);
-        queue.add(startPosition);
-
-        while (!queue.isEmpty()) {
-            int[] actualPosition = queue.remove();
-
-            for (int[] adjacent: NEIGHBOURS) {
-                int visitedX = actualPosition[0]+adjacent[0];
-                int visitedY = actualPosition[1]+adjacent[1];
-
-                try {
-                    if (!visited[visitedX][visitedY] && state[visitedX][visitedY] == state[startX][startY]) {
-                        int[] visitedPosition = {visitedX, visitedY};
-
-                        sameChain.add(visitedPosition);
-                        queue.add(visitedPosition);
-                    }
-
-                    visited[visitedX][visitedY] = true;
-                } catch (ArrayIndexOutOfBoundsException ex) {
-                    // it's OK
-                }
-            }
-        }
-
-        return sameChain;
-    }
-
     private static boolean hasLiberty(int x, int y, Color[][] state) {
-        List<int[]> chain = getChain(x, y, state);
+        List<int[]> chain = Utils.getChain(x, y, state);
         for (int[] stone : chain) {
-            for (int[] adjacent : NEIGHBOURS) {
+            for (int[] adjacent : Utils.getNeighbours(stone)) {
                 try {
-                    if (state[stone[0] + adjacent[0]][stone[1] + adjacent[1]] == Color.NONE) {
+                    if (state[adjacent[0]][adjacent[1]] == Color.NONE) {
                         return true;
                     }
 
---
+++ common/src/main/java/gogame/common/Utils.java
@@ -0,0 +1,52 @@
+package gogame.common;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class Utils {
+    public static int[][] getNeighbours(int[] position) {
+            return new int[][]{
+                    {position[0]-1, position[1]},
+                    {position[0]+1, position[1]},
+                    {position[0], position[1]-1},
+                    {position[0], position[1]+1}
+            };
+    }
+
+    public static List<int[]> getChain(int startX, int startY, Color[][] state) {
+        boolean[][] visited = new boolean[state.length][state[0].length];
+        List<int[]> sameChain = new ArrayList<int[]>();
+        Queue<int[]> queue = new LinkedList<int[]>();
+        int[] startPosition = {startX, startY};
+
+        visited[startX][startY] = true;
+        sameChain.add(startPosition);
+        queue.add(startPosition);
+
+        while (!queue.isEmpty()) {
+            int[] actualPosition = queue.remove();
+
+            for (int[] adjacent: getNeighbours(actualPosition)) {
+                int visitedX = adjacent[0];
+                int visitedY = adjacent[1];
+
+                try {
+                    if (!visited[visitedX][visitedY] && state[visitedX][visitedY] == state[startX][startY]) {
+                        int[] visitedPosition = {visitedX, visitedY};
+
+                        sameChain.add(visitedPosition);
+                        queue.add(visitedPosition);
+                    }
+
+                    visited[visitedX][visitedY] = true;
+                } catch (ArrayIndexOutOfBoundsException ex) {
+                    // it's OK
+                }
+            }
+        }
+
+        return sameChain;
+    }
+}
